f356b86eb64c23649bf5fb2ad545fba4c547791c
Yunie <s4001725@student.rmit.edu.au>
2024-10-19 22:59:18 +1100

    constructior for Prescription

---
+++ Prescription.java
@@ -0,0 +1,22 @@
+public class Prescription {
+    private int prescID;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private float sphere;
+    private float axis;
+    private float cylinder;
+    private Date examinationDate;
+    private String optometrist;
+    private String[] remarkTypes = {"Client", "Optometrist"};
+    private ArrayList<String> postRemarks = new ArrayList<>();
+
+    public
+    public boolean addPrescription() {
+
+    }
+
+    public boolean addRemark() {
+    return true;
+    }
+}
